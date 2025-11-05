0b50a75b1f8f8b32a89213f2d66da5451c56cede
ClodaghMulvihill <clodaghmu@gmail.com>
2025-11-05 17:44:21 +0000

    CM - hardcoded modules

--- src/main/model/Module.java
+++ src/main/model/Module.java
@@ -1,11 +1,23 @@
 package main.model;
 
+import java.util.Arrays;
+
 public enum Module {
 
-    dsc("Data Structures and Concurrencies", GradingPresets.)
+    dsc("Data Structures and Concurrency", (GradingPresets) GradingPresets.projectAndFinal()),
+    wf("Web Frameworks", (GradingPresets) GradingPresets.projectAndFinal()),
+    ooad("Object Oriented Analysis and Design", (GradingPresets) GradingPresets.custom(Arrays.asList(0.3, 0.3, 0.4))),
+    st("Software Tools", (GradingPresets) GradingPresets.projectAndFinal()),
+    da("Data Analytics", (GradingPresets) GradingPresets.custom(Arrays.asList(0.5, 0.15, 0.35))),
+    pd("Professional Development", (GradingPresets) GradingPresets.custom(Arrays.asList(0.4, 0.3, 0.3)));
 
     private final String name;
     private final GradingPresets gradePreset;
+
+    Module(String name, GradingPresets gradePreset) {
+        this.name = name;
+        this.gradePreset = gradePreset;
+    }
 
 
 }
