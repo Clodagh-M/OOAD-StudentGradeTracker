da4639b81540ea2096f26bad560985fdea35cd23
StudentWiktoriaDepta <wiktoriadepta1@gmail.com>
2025-11-12 16:32:54 +0000

    WD - Module Class

--- src/main/model/Module.java
+++ src/main/model/Module.java
@@ -1,4 +1,34 @@
 package main.model;
 
+import java.util.ArrayList;
+
 public class Module {
+    private String name;
+    private ArrayList<Assignment> assignments;
+
+    public Module(String name, ArrayList<Assignment> assignments) {
+        this.name = name;
+        this.assignments = assignments;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Assignment> getAssignments() {
+        return assignments;
+    }
+
+    public void setAssignments(ArrayList<Assignment> assignments) {
+        this.assignments = assignments;
+    }
+
+    public void addModule(Assignment assignment) {
+        this.assignments.add(assignment);
+    }
+
 }
