251ccd5f10d4c90bc9c3b554469139c3156637e2
ClodaghMulvihill <clodaghmu@gmail.com>
2025-11-12 16:29:12 +0000

    CM - Course class

--- src/main/model/Course.java
+++ src/main/model/Course.java
@@ -1,4 +1,28 @@
 package main.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
+    private String name;
+    private List<Module> modules;
+
+    public Course(String name, List<Module> modules) {
+        this.name = name;
+        this.modules = new ArrayList<>();
+    }
+
+    public void addModule(Module module) {
+        modules.add(module);
+    }
+
+    public double getOverallGrade() {
+        double totalWeighted = 0;
+        double totalWeight = 0;
+        for (Module module : modules) {
+            totalWeighted += module.getModuleGrade() * (module.getWeight() / 100);
+            totalWeight += module.getWeight();
+        }
+        return totalWeight > 0 ? (totalWeight / totalWeight) * 100 : 0;
+    }
 }
