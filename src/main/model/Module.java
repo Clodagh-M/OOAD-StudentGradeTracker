805260a18f48cfc4ee487d11f76b4e78a64c926c
ClodaghMulvihill <clodaghmu@gmail.com>
2025-11-12 17:08:33 +0000

    CM - basic functionality

--- src/main/model/Module.java
+++ src/main/model/Module.java
@@ -8,7 +8,12 @@ public class Module {
 
     public Module(String name, ArrayList<Assignment> assignments) {
         this.name = name;
-        this.assignments = assignments;
+        this.assignments = assignments != null ? assignments : new ArrayList<>();
+    }
+
+    public Module(String name) {
+        this.name = name;
+        this.assignments = new ArrayList<>();
     }
 
     public String getName() {
@@ -23,11 +28,25 @@ public class Module {
         return assignments;
     }
 
+    public double getModuleGrade() {
+        if (assignments.isEmpty()) return 0;
+
+        double totalWeighted = 0;
+        double totalWeight = 0;
+
+        for (Assignment assignment : assignments) {
+            totalWeighted += assignment.getPercentage() * (assignment.getWeight() / 100);
+            totalWeight += assignment.getWeight();
+        }
+
+        return totalWeight > 0 ? totalWeighted : 0;
+    }
+
     public void setAssignments(ArrayList<Assignment> assignments) {
         this.assignments = assignments;
     }
 
-    public void addModule(Assignment assignment) {
+    public void addAssignment(Assignment assignment) {
         this.assignments.add(assignment);
     }
 
