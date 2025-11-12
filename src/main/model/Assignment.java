f3f5eccd1af49861508621013951dc1ea72db675
StudentKarenAmumpair <karenamumpaire0@gmail.com>
2025-11-12 16:32:23 +0000

    KA Assigment class

--- src/main/model/Assignment.java
+++ src/main/model/Assignment.java
@@ -1,4 +1,40 @@
 package main.model;
 
 public class Assignment {
-}
+    private String name;
+
+    private double score;
+
+    private double maxScore;
+
+    private double weight;
+    public Assignment(String name, double score, double maxScore, double weight) {
+        this.name = name;
+        this.score = score;
+        this.maxScore = maxScore;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getScore() {
+        return score;
+    }
+    public void setScore(double score) {
+        this.score = score;
+    }
+    public double getMaxScore() {
+        return maxScore;
+    }
+    public void setMaxScore(double maxScore) {
+        this.maxScore = maxScore;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }}
+
