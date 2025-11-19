e4339f51be6c6cae5796148ceff89b16c0bd7a88
StudentKarenAmumpair <karenamumpaire0@gmail.com>
2025-11-19 17:23:00 +0000

    KA

--- src/main/ui/GradeTrackerUI.java
+++ src/main/ui/GradeTrackerUI.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import main.model.*;
 import main.model.Module;
 
-import static main.model.Assignment.calculateGradeLeft;
+
 
 public class GradeTrackerUI {
     private Course course;
@@ -79,6 +79,41 @@ public class GradeTrackerUI {
             }
             System.out.println();
         }
+    }
+    private void calculateGradeLeft(Module module, Scanner scanner){
+         double scoreWithWeight = 0;
+         double totalweight=0;
+
+        System.out.println("Please enter grade of  each assignment: enter zero if not completed "+module.getName());
+       for(Assignment assignment : module.getAssignments()){
+           System.out.printf("  - %s: %.1f/100\n",assignment.getName(),assignment.getScore());
+           double score = scanner.nextDouble();
+           scanner.nextLine();
+           assignment.setScore(score);
+           if (score==0){
+               System.out.println("Only assigments enntered before zero considered");
+               break;
+           }
+            scoreWithWeight += score*(assignment.getWeight()/100);
+            totalweight += assignment.getWeight();
+       }
+
+
+
+
+        double remainingWeight = 100 - totalweight;
+        double remainingWeightPercent = remainingWeight/100;
+        double passmark=40;
+        double finalgrade=(passmark-scoreWithWeight)/remainingWeightPercent;
+        if(finalgrade<=0){
+            System.out.printf("You  have already passed the module with current assignment score");
+        }
+        else if(finalgrade>100){
+            System.out.printf("Its is impossible to pass the module with current assignment score");
+        }
+
+
+        System.out.printf("You need at least %.2f%% of the remaining assessment(s) to pass overall .%n",finalgrade);
     }
 
     private void updateAssignmentScore() {
@@ -164,17 +199,13 @@ public class GradeTrackerUI {
                     System.out.print("Select  a module: ");
                     int MODULEINDEX= scanner.nextInt() - 1;
                     scanner.nextLine();
+                    if(MODULEINDEX < 0 || MODULEINDEX >= course.getModules().size()){
+                        System.out.println("Invalid module selection!");
+                        return;
+                    }
                     Module selected=course.getModules().get(MODULEINDEX);
-                   for(int i=0;i<selected.getAssignments().size();i++){
-                       Assignment assignment=selected.getAssignments().get(i);
-                       System.out.printf("%d. %s (weight %f.2)\n",i+1,assignment.getName(),assignment.getWeight());
-                   }
-                    System.out.print("Select assignment: ");
-                    int assigmentindex= scanner.nextInt() - 1;
-                    scanner.nextLine();
-                    Assignment selectedAssignment=selected.getAssignments().get(assigmentindex);
 
-                    calculateGradeLeft(selectedAssignment,scanner);
+                    calculateGradeLeft(selected,scanner);
                     break;
 
                 case 5:
