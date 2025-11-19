9e6df36a0c5cb03743b603192c77b616e98875ef
StudentKarenAmumpair <karenamumpaire0@gmail.com>
2025-11-19 17:25:48 +0000

    assigmentserviceKA

--- src/main/service/AssignmentCalculator.java
+++ src/main/service/AssignmentCalculator.java
@@ -1,6 +1,44 @@
 package main.service;
 
 import main.model.Assignment;
+import main.model.Module;
+
+import java.util.Scanner;
 
 public class AssignmentCalculator {
+    public static void  calculateGradeLeft(Module module, Scanner scanner){
+        double scoreWithWeight = 0;
+        double totalweight=0;
+
+        System.out.println("Please enter grade of  each assignment: enter zero if not completed "+module.getName());
+        for(Assignment assignment : module.getAssignments()){
+            System.out.printf("  - %s: %.1f/100\n",assignment.getName(),assignment.getScore());
+            double score = scanner.nextDouble();
+            scanner.nextLine();
+            assignment.setScore(score);
+            if (score==0){
+                System.out.println("Only assigments enntered before zero considered");
+                break;
+            }
+            scoreWithWeight += score*(assignment.getWeight()/100);
+            totalweight += assignment.getWeight();
+        }
+
+
+
+
+        double remainingWeight = 100 - totalweight;
+        double remainingWeightPercent = remainingWeight/100;
+        double passmark=40;
+        double finalgrade=(passmark-scoreWithWeight)/remainingWeightPercent;
+        if(finalgrade<=0){
+            System.out.printf("You  have already passed the module with current assignment score");
+        }
+        else if(finalgrade>100){
+            System.out.printf("Its is impossible to pass the module with current assignment score");
+        }
+
+
+        System.out.printf("You need at least %.2f%% of the remaining assessment(s) to pass overall .%n",finalgrade);
+    }
 }
--- src/main/ui/GradeTrackerUI.java
+++ src/main/ui/GradeTrackerUI.java
@@ -6,6 +6,7 @@ import main.model.*;
 import main.model.Module;
 import main.service.CourseCalculator;
 
+import static main.service.AssignmentCalculator.calculateGradeLeft;
 
 
 public class GradeTrackerUI {
@@ -80,41 +81,7 @@ public class GradeTrackerUI {
             System.out.println();
         }
     }
-    private void calculateGradeLeft(Module module, Scanner scanner){
-         double scoreWithWeight = 0;
-         double totalweight=0;
 
-        System.out.println("Please enter grade of  each assignment: enter zero if not completed "+module.getName());
-       for(Assignment assignment : module.getAssignments()){
-           System.out.printf("  - %s: %.1f/100\n",assignment.getName(),assignment.getScore());
-           double score = scanner.nextDouble();
-           scanner.nextLine();
-           assignment.setScore(score);
-           if (score==0){
-               System.out.println("Only assigments enntered before zero considered");
-               break;
-           }
-            scoreWithWeight += score*(assignment.getWeight()/100);
-            totalweight += assignment.getWeight();
-       }
-
-
-
-
-        double remainingWeight = 100 - totalweight;
-        double remainingWeightPercent = remainingWeight/100;
-        double passmark=40;
-        double finalgrade=(passmark-scoreWithWeight)/remainingWeightPercent;
-        if(finalgrade<=0){
-            System.out.printf("You  have already passed the module with current assignment score");
-        }
-        else if(finalgrade>100){
-            System.out.printf("Its is impossible to pass the module with current assignment score");
-        }
-
-
-        System.out.printf("You need at least %.2f%% of the remaining assessment(s) to pass overall .%n",finalgrade);
-    }
 
     private void updateAssignmentScore() {
         System.out.println("\n=== Update Assignment Score ===");
